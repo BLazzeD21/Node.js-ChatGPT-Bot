fac976e1fdb3578b3198edf608d0b7cfbba28bd2
Alexandr Sekerin <blazzed21@gmail.com>
2023-04-26 02:35:13 +0300

    Added code for bot

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
+
+import config from 'config'
+import { openai } from './openai.js'
+
+const INITIAL_SESSION = {
+    messages: [],
+}
+const bot = new Telegraf(config.get('BOT_TOKEN'))
+
+bot.use(session())
+
+bot.command('start', async (ctx) => {
+    ctx.session = INITIAL_SESSION
+    await ctx.reply("Hello, welcome to an artificial intelligence chatbot that will help you with everything! 🤖\n\nYou can find the source code of the bot here:\nhttps://github.com/BLazzeD21/Node.js-ChatGPT-Bot");
+})
+
+bot.command('new', async (ctx) => {
+    ctx.session = INITIAL_SESSION
+    await ctx.reply('You have started a new dialogue with the bot. 🤖\nGood luck with your use!')
+})
+bot.on(message('text'), async (ctx) => {
+    try {
+        await ctx.reply(code("Text accepted for processing"))
+        const userId = String(ctx.message.from.id)
+        const text = ctx.message.text
+        ctx.session.messages.push({
+            role: openai.roles.USER,
+            content: text
+        })
+        const response = await openai.chat(ctx.session.messages)
+
+        ctx.session.messages.push({
+            role: openai.roles.ASSISTANT, 
+            content: response.content
+        })
+        await ctx.reply(response.content)
+    } catch(error) {
+        await ctx.reply("You need to create a new session: /new\nIf that doesn't help, just wait a bit! 🔧")
+    }
+})
+
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
---
+++ src/openai.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai"
+import config from 'config'
+
+class OpenAI {
+    roles = {
+        ASSISTANT: 'assistant',
+        USER: 'user',
+        SYSTEM: 'system',
+    }
+
+    constructor(apiKey) {
+        const configuration = new Configuration({
+            apiKey,
+          });
+        this.openai = new OpenAIApi(configuration);
+    }
+        
+    async chat(messages) {
+        try {
+            const response = await this.openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages
+            })
+
+            return response.data.choices[0].message
+        } catch (error) {
+            console.log('Error getting response')
+        }
+    }
+}
+
+export const openai = new OpenAI(config.get('OPENAI_KEY'))
