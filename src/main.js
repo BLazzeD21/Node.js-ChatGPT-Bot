44e1c1882d5c50b9f11604d0575661adcb82f482
Alexandr Sekerin <blazzed21@gmail.com>
2023-04-29 01:47:41 +0300

    Added voice messages handler

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { Telegraf, session } from 'telegraf'
 import { message } from 'telegraf/filters'
 import { code } from 'telegraf/format'
 
+import { converter } from './converter.js'
 import config from 'config'
 import { openai } from './openai.js'
 
@@ -15,7 +16,7 @@ const createInitialSession = () => {
   return {
     messages: []
   }
-}
+};
 
 bot.command('start', async (ctx) => {
   const sessionId = ctx.message.chat.id;
@@ -32,6 +33,36 @@ bot.command('new', async (ctx) => {
   const sessionId = ctx.message.chat.id;
   sessions[sessionId] = createInitialSession();
   await ctx.reply('The context has been reset.');
+});
+
+bot.on(message('voice'), async (ctx) => {
+  const sessionId = ctx.message.chat.id;
+  sessions[sessionId] ??= createInitialSession();
+  try {
+    await ctx.reply(code("Text accepted for processing"))
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    const oggPath = await converter.create(link.href, userId)
+    const mp3Path = await converter.toMp3(oggPath, userId)
+    const text = await openai.transcription(mp3Path)
+    sessions[sessionId].messages.push({
+      role: openai.roles.USER,
+      content: text
+    })
+
+    const response = await openai.chat(sessions[sessionId].messages)
+
+    setTimeout(async () => {
+      sessions[sessionId].messages.push({
+        role: openai.roles.ASSISTANT, 
+        content: response.content
+      })
+      await ctx.reply(response.content)
+    }, 5000)
+
+  } catch (error) {
+    console.log("Voice error " + error.message)
+  }
 });
 
 bot.on(message('text'), async (ctx) => {
@@ -55,8 +86,8 @@ bot.on(message('text'), async (ctx) => {
       await ctx.reply(response.content)
     }, 5000)
 
-
   } catch(error) {
+    console.log("Text error " + error.message)
     await ctx.reply("Sorry, no response received from the server")
   }
 });
