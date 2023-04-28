46e6457649f8cc935fd06a555951cd3a3736d775
Alexandr Sekerin <blazzed21@gmail.com>
2023-04-29 01:44:09 +0300

    Function to delete temporary ogg files

---
+++ src/deleteFile.js
@@ -0,0 +1,9 @@
+import { unlink } from 'fs/promises'
+
+export async function deleteFile(path) {
+    try {
+        await unlink(path)
+    } catch (error) {
+        console.log('Error delete ' + error.message)
+    }
+}
