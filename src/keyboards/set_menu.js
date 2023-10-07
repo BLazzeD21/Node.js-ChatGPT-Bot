16e40ea36ef2c9cb82658c52266e9035dd955c6a
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-07 22:55:24 +0300

    Function for setting commands for the bot

---
+++ src/keyboards/set_menu.js
@@ -0,0 +1,11 @@
+import { commands } from '../lexicon/lexicon_en.js'
+
+export const setMenu = async (bot)  => {
+  bot.telegram.setMyCommands(commands)
+  .then(() => {
+    console.log('Custom commands set successfully');
+  })
+  .catch((error) => {
+    console.error('Error setting custom commands:', error);
+  });
+}
