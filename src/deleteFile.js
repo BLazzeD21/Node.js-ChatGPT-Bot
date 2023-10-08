b4b9663dceb01203d896af7304ac69a2866a0ddd
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-08 22:51:52 +0300

    Files moved and renamed

--- src/deleteFile.js
+++
@@ -1,9 +0,0 @@
-import { unlink } from 'fs/promises'
-
-export async function deleteFile(path) {
-    try {
-        await unlink(path)
-    } catch (error) {
-        console.log('Error delete ' + error.message)
-    }
-}
--- src/deleteWebhook.js
+++
@@ -1,9 +0,0 @@
-export const deleteWebHook = async (bot)  => {
-  bot.telegram.deleteWebhook()
-  .then(() => {
-    console.log('Webhook deleted successfully');
-  })
-  .catch((error) => {
-    console.error('Error deleting webhook:', error);
-  });
-}
