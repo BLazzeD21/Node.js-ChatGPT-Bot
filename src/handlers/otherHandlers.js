981913069bd44098b40bacf1e3a55e9bc0e63cc0
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-08 22:52:36 +0300

    Added handlers for text and voice

---
+++ src/handlers/otherHandlers.js
@@ -0,0 +1,80 @@
+import { code } from 'telegraf/format';
+import { LEXICON_EN, printError } from '../lexicon/lexicon_en.js';
+
+import { openai } from '../openai.js';
+import { converter } from '../converter.js';
+
+import { createInitialSession } from '../utils/createSession.js';
+import { checkAccess } from '../utils/checkAccess.js';
+
+export const textHandler = (allowedUserId, sessions) => {
+  return async (ctx) => {
+    if (await checkAccess(allowedUserId, ctx)) return;
+
+    const sessionId = ctx.message.chat.id;
+    sessions[sessionId] ??= createInitialSession();
+    try {
+      const processing = await ctx.reply(code(LEXICON_EN['processing']));
+      const text = ctx.message.text;
+      sessions[sessionId].messages.push({
+        role: openai.roles.USER,
+        content: text,
+      });
+
+      const response = await openai.chat(sessions[sessionId].messages);
+      await ctx.deleteMessage(processing.message_id);
+      setTimeout(async () => {
+        if (response && response.content) {
+          sessions[sessionId].messages.push({
+            role: openai.roles.ASSISTANT,
+            content: response.content,
+          });
+          await ctx.reply(response.content);
+        } else {
+          await ctx.reply(LEXICON_EN['manyRequests']);
+        }
+      }, 2000);
+    } catch (error) {
+      console.log(printError(error));
+      await ctx.reply(LEXICON_EN['noResponce']);
+    }
+  };
+};
+
+export const voiceHandler = (allowedUserId, sessions) => {
+  return async (ctx) => {
+    if (await checkAccess(allowedUserId, ctx)) return;
+
+    const sessionId = ctx.message.chat.id;
+    sessions[sessionId] ??= createInitialSession();
+    try {
+      const processing = await ctx.reply(code(LEXICON_EN['processing']));
+      const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+      const userId = String(ctx.message.from.id);
+      const oggPath = await converter.create(link.href, userId);
+      const mp3Path = await converter.toMp3(oggPath, userId);
+      const text = await openai.transcription(mp3Path);
+      sessions[sessionId].messages.push({
+        role: openai.roles.USER,
+        content: text,
+      });
+
+      const response = await openai.chat(sessions[sessionId].messages);
+      await ctx.deleteMessage(processing.message_id);
+      setTimeout(async () => {
+        if (response && response.content) {
+          sessions[sessionId].messages.push({
+            role: openai.roles.ASSISTANT,
+            content: response.content,
+          });
+          await ctx.reply(response.content);
+        } else {
+          await ctx.reply(LEXICON_EN['manyRequests']);
+        }
+      }, 2000);
+    } catch (error) {
+      console.log(printError(error));
+      await ctx.reply(LEXICON_EN['noResponce']);
+    }
+  };
+};
