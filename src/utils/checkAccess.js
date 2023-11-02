bf01cdd96c4696269aa6540778a42d83d5174758
Alexandr Sekerin <blazzed21@gmail.com>
2023-11-03 01:00:10 +0300

    Updated user verification function

--- src/utils/checkAccess.js
+++ src/utils/checkAccess.js
@@ -1,6 +1,15 @@
 import { LEXICON_EN } from '../lexicon/lexicon_en.js';
 
-export async function checkAccess(allowedUserId, ctx) {
+export async function getUsersArray(config) {
+  return config
+    .USERS_ID
+    .split(",")
+    .map((id) => parseInt(id));
+}
+
+export async function checkAccess(config, ctx) {
+  const allowedUserId = await getUsersArray(config);
+
   if (!allowedUserId.includes(ctx.from.id)) {
     await ctx.reply(LEXICON_EN['deniedAccess']);
     return true;
