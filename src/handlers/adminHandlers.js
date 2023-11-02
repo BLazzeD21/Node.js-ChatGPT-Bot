0db915f8b49c89777d93adfdbc9aec337568f87d
Alexandr Sekerin <blazzed21@gmail.com>
2023-11-03 01:01:49 +0300

    Added two admin commands

---
+++ src/handlers/adminHandlers.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import { LEXICON_EN } from "../lexicon/lexicon_en.js";
+
+export const addHandler = (config, callback) => {
+  return async (ctx) => {
+    if (ctx.from.id == config.SUPER_USER) {
+      try {
+        const requestText = ctx.message.text
+          .replace("/add", "")
+          .trim();
+        
+        if (!requestText){
+          await ctx.reply(LEXICON_EN['emptyAdd'], { parse_mode: "HTML" });
+          return;
+        }
+
+        config.USERS_ID += `,${requestText}`;
+
+        fs.writeFileSync('config/production.json', JSON.stringify(config), 'utf8');
+        callback(config);
+
+        ctx.reply(LEXICON_EN['add']);
+        ctx.telegram.sendMessage(requestText, LEXICON_EN['added']);
+      } catch(e) {
+        console.log(LEXICON_EN['errorSending'], requestText);
+      }
+    } else {
+        ctx.reply(LEXICON_EN['super']);
+    }
+  }
+};
+
+export const showHandler = (config) => {
+  return async (ctx) => {
+    if (ctx.from.id == config.SUPER_USER) {
+      await ctx.reply(config.USERS_ID);
+    } else {
+      ctx.reply(LEXICON_EN['super']);
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,52 +1,58 @@
 import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters";
-import config from "config";
+import fs from 'fs';
 
+import { getUsersArray } from "./utils/checkAccess.js";
 import { LEXICON_EN } from "./lexicon/lexicon_en.js";
 import { setMenu } from "./keyboards/set_menu.js";
 import { deleteWebHook } from "./utils/deleteWebhook.js";
 import { sendMessages } from "./utils/sendMessages.js";
 
 import {
-  startHandler,
-  helpHandler,
-  chatIDHandler,
-  newHandler,
-  imageHandler,
-  passwordHandler,
+  startHandler, helpHandler, chatIDHandler,
+  newHandler, imageHandler, passwordHandler,
 } from "./handlers/userHandlers.js";
 import { textHandler, voiceHandler } from "./handlers/otherHandlers.js";
+import { addHandler, showHandler } from "./handlers/adminHandlers.js";
 
 const sessions = {};
 
-const bot = new Telegraf(config.get("BOT_TOKEN"));
+let config = JSON.parse(fs.readFileSync('config/production.json', 'utf8'));
 
-const allowedUserId = config
-  .get("USERS_ID")
-  .split(",")
-  .map((id) => parseInt(id));
+const bot = new Telegraf(config.BOT_TOKEN);
+
+
+const updateConfigValue = (updatedConfig) => {
+  config = updatedConfig;
+};
 
 bot.use(session());
 
 bot.command("start", startHandler(sessions));
-bot.command("new", newHandler(allowedUserId, sessions));
-bot.command("help", helpHandler(allowedUserId));
-bot.command("image", imageHandler(allowedUserId));
+
+bot.command("add", addHandler(config, updateConfigValue));
+
+bot.command("show", showHandler(config));
+
+bot.command("new", newHandler(config, sessions));
+bot.command("help", helpHandler(config));
+bot.command("image", imageHandler(config));
 bot.command("chatid", chatIDHandler());
 bot.command("password", passwordHandler());
 
 bot.hears(LEXICON_EN["getIDs_btn"], chatIDHandler());
 bot.hears(LEXICON_EN["password_btn"], passwordHandler());
-bot.hears(LEXICON_EN["reset_btn"], newHandler(allowedUserId, sessions));
+bot.hears(LEXICON_EN["reset_btn"], newHandler(config, sessions));
 
-bot.on(message("text"), textHandler(allowedUserId, sessions));
-bot.on(message("voice"), voiceHandler(allowedUserId, sessions));
+bot.on(message("text"), textHandler(config, sessions));
+bot.on(message("voice"), voiceHandler(config, sessions));
+
 
 setMenu(bot);
 deleteWebHook(bot);
 
-bot.launch({ dropPendingUpdates: true })
-  .then(sendMessages(bot, allowedUserId));
-
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+bot.launch({ dropPendingUpdates: true })
+ .then(sendMessages(bot, await getUsersArray(config)));
