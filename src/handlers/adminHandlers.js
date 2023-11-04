42ae2b3f5d3dcac77ebd9b7a5cd75eac16550eec
Alexandr Sekerin <blazzed21@gmail.com>
2023-11-04 14:55:38 +0300

    feat:  added command to view list of users

--- src/handlers/adminHandlers.js
+++ src/handlers/adminHandlers.js
@@ -1,61 +1,74 @@
 import fs from 'fs';
-import { LEXICON_EN } from "../lexicon/lexicon_en.js";
+import { LEXICON_EN } from '../lexicon/lexicon_en.js';
 import { getUsersArray } from '../utils/checkAccess.js';
-import { existUser, getExistsUsers } from '../utils/usersCheck.js';
 
 export const addHandler = (config, callback) => {
   return async (ctx) => {
     if (ctx.from.id == config.SUPER_USER) {
       try {
-        const requestText = ctx.message.text
-          .replace("/add", "")
-          .trim();
-        
-        if (!requestText){
-          await ctx.reply(LEXICON_EN['emptyAdd'], { parse_mode: "HTML" });
-          return;
-        }
+        const requestText = parseInt(ctx.message.text
+            .replace('/add', '')
+            .trim());
 
-        const userExistence = await existUser(requestText, ctx);
-
-        if (!userExistence){
-          await ctx.reply(LEXICON_EN['noUser'], { parse_mode: "HTML" });
+        if (!requestText) {
+          await ctx.reply(LEXICON_EN['emptyAdd'], { parse_mode: 'HTML' });
           return;
         }
 
         config.USERS_ID += `,${requestText}`;
 
-        if (process.env.NODE_ENV === "production") {
-          fs.writeFileSync('config/production.json', JSON.stringify(config, null, 2), 'utf8');
+        if (process.env.NODE_ENV === 'production') {
+          fs.writeFileSync('config/production.json',
+              JSON.stringify(config, null, 2), 'utf8');
         } else {
-          fs.writeFileSync('config/default.json', JSON.stringify(config, null, 2), 'utf8');
+          fs.writeFileSync('config/default.json',
+              JSON.stringify(config, null, 2), 'utf8');
         }
 
         await callback(config);
 
-        await ctx.reply(LEXICON_EN['add']);
-        await ctx.telegram.sendMessage(requestText, LEXICON_EN['added']);
+        const output =`<b>Added ID: </b><code>${requestText}</code>\n\n` +
+        `${LEXICON_EN['add']}`;
 
-      } catch(e) {
+        ctx.reply(output, { parse_mode: 'HTML' });
+      } catch (e) {
         console.log(LEXICON_EN['errorSending'], requestText);
       }
     } else {
-        await ctx.reply(LEXICON_EN['super']);
+      await ctx.reply(LEXICON_EN['super']);
     }
-  }
+  };
 };
 
 export const showHandler = (config) => {
   return async (ctx) => {
     if (ctx.from.id == config.SUPER_USER) {
       const users = await getUsersArray(config);
-      let usersExists = await getExistsUsers(users, ctx);
-      usersExists = usersExists.slice(0, -1);
 
-      config.USERS_ID = usersExists;
-      console.log(config);
+      const uniqueIDs = [...new Set(users)];
+
+      config.USERS_ID = uniqueIDs.join(',');
+
+      if (process.env.NODE_ENV === 'production') {
+        fs.writeFileSync('config/production.json',
+            JSON.stringify(config, null, 2), 'utf8');
+      } else {
+        fs.writeFileSync('config/default.json',
+            JSON.stringify(config, null, 2), 'utf8');
+      }
+
+      let output = '🗃 <b>Bot users:</b>\n\n';
+
+      uniqueIDs.forEach(
+          (id) => {
+            output += `👤 <code>${id}</code>\n`;
+          });
+
+      await ctx.reply(output, { parse_mode: 'HTML' });
+
+      await callback(config);
     } else {
       await ctx.reply(LEXICON_EN['super']);
     }
-  }
-}
+  };
+};
