605ba8d96aec5a65733a2781947ef8d032a711e0
Alexandr Sekerin <blazzed21@gmail.com>
2023-11-04 17:59:51 +0300

    fix: checking for an empty array with users

--- src/utils/checkAccess.js
+++ src/utils/checkAccess.js
@@ -1,10 +1,17 @@
 import { LEXICON_EN } from '../lexicon/lexicon_en.js';
 
 export async function getUsersArray(config) {
-  return config
+  let usersArray = config
       .USERS_ID
       .split(',')
       .map((id) => parseInt(id));
+
+  if (usersArray.includes(NaN)) {
+    usersArray = [config.SUPER_USER];
+  }
+
+  console.log(usersArray);
+  return usersArray;
 }
 
 export async function checkAccess(config, ctx) {
