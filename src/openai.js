fd504a5ee82b6d230a5aff59f84ecfd7de080e3c
Alexandr Sekerin <blazzed21@gmail.com>
2023-11-25 23:44:12 +0300

    chore: openai methods rewritten in the new version

--- src/openai.js
+++ src/openai.js
@@ -1,50 +1,55 @@
+import { OpenAI } from 'openai';
 
-import { Configuration, OpenAIApi } from 'openai';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import config from 'config';
 import { createReadStream } from 'fs';
 
-class OpenAI {
+class OpenAIApi {
   roles = {
     ASSISTANT: 'assistant',
     USER: 'user',
     SYSTEM: 'system',
   };
 
-  constructor(apiKey) {
-    const configuration = new Configuration({
-      apiKey,
+  constructor(apiKey, proxyUrl) {
+    this.openai = new OpenAI({
+      maxRetries: 0,
+      apiKey: apiKey,
+      httpAgent: new HttpsProxyAgent(proxyUrl),
     });
-    this.openai = new OpenAIApi(configuration);
-  }
-
-  async transcription(filepath) {
-    const response = await this.openai.createTranscription(
-        createReadStream(filepath),
-        'whisper-1',
-    );
-    return response.data.text;
   }
 
   async chat(messages) {
-    const response = await this.openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
+    const response = await this.openai.chat.completions.create({
       messages,
+      model: 'gpt-3.5-turbo',
     });
 
-    return response.data.choices[0].message;
+    return response.choices[0].message;
+  }
+
+  async transcription(filepath) {
+    const response = await this.openai.audio.transcriptions.create({
+      file: createReadStream(filepath),
+      model: 'whisper-1',
+    });
+    return response.text;
   }
 
   async getImage(text, size, count) {
-    const response = await this.openai.createImage({
+    const response = await this.openai.images.generate({
       prompt: text,
       n: count,
       size: size,
     });
 
-    const imageUrl = response.data.data[0].url;
+    const imageUrl = response.data[0].url;
 
     return imageUrl;
   }
 }
 
-export const openai = new OpenAI(config.get('OPENAI_KEY'));
+export const openai = new OpenAIApi(
+    config.get('OPENAI_KEY'),
+    config.get('PROXY_URL'),
+);
