159fe3110f3e4c31455249508ace65fcad6d9e65
Alexandr Sekerin <blazzed21@gmail.com>
2024-01-18 21:14:55 +0300

    fix: fix sending messages when starting the bot

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import { SessionStore } from "telegraf";
-import { Context, Telegraf, session } from "telegraf";
-import { Redis } from "@telegraf/session/redis";
-import { limit } from "@grammyjs/ratelimiter";
-import fs from "fs";
+import { SessionStore } from 'telegraf';
+import { Context, Telegraf, session } from 'telegraf';
+import { Redis } from '@telegraf/session/redis';
+import { limit } from '@grammyjs/ratelimiter';
+import fs from 'fs';
 
-import { registerHandlers } from "./handlers/handlersRegistration.js";
-import { getUsersArray } from "./utils/checkAccess.js";
-import { sendMessages } from "./utils/sendMessages.js";
-import { LEXICON_EN } from "./lexicon/lexicon_en.js";
-import { setMenu } from "./keyboards/set_menu.js";
-import { deleteWebHook } from "./utils/deleteWebhook.js";
+import { registerHandlers } from './handlers/handlersRegistration.js';
+import { sendMessages } from './utils/sendMessages.js';
+import { LEXICON_EN } from './lexicon/lexicon_en.js';
+import { setMenu } from './keyboards/set_menu.js';
+import { deleteWebHook } from './utils/deleteWebhook.js';
+
+interface MyContext extends Context {
+  myProp?: string
+  myOtherProp?: number
+}
 
 let config: Config =
-  process.env.NODE_ENV === "production"
-    ? JSON.parse(fs.readFileSync("config/production.json", "utf8"))
-    : JSON.parse(fs.readFileSync("config/default.json", "utf8"));
+  process.env.NODE_ENV === 'production' ?
+    JSON.parse(fs.readFileSync('config/production.json', 'utf8')) :
+    JSON.parse(fs.readFileSync('config/default.json', 'utf8'));
 
 const redisStorage: SessionStore<any> = Redis({
   url: config.REDIS_URL,
@@ -28,11 +32,11 @@ const requestslimit: (ctx: Context, next: Function) => Promise<void> = limit({
   timeFrame: 60000,
   limit: 3,
   onLimitExceeded: (ctx: Context) => {
-    ctx.reply(LEXICON_EN["tooManyRequests"]);
+    ctx.reply(LEXICON_EN['tooManyRequests']);
   },
 });
 
-const bot: Telegraf = new Telegraf(config.BOT_TOKEN, {
+const bot: Telegraf = new Telegraf<MyContext>(config.BOT_TOKEN, {
   handlerTimeout: 180_000,
 });
 
@@ -40,17 +44,16 @@ bot.use(session({ store: redisStorage }));
 
 registerHandlers(bot, config, redisStorage, updateConfigValue, requestslimit);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   setMenu(bot);
   deleteWebHook(bot);
 }
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-bot.launch({ dropPendingUpdates: true }).then(async () => {
-  if (process.env.NODE_ENV === "production") {
-    const usersArray = await getUsersArray(config);
-    await sendMessages(bot, usersArray);
-  }
+bot.launch({ dropPendingUpdates: true });
+
+bot.telegram.getMe().then(async () => {
+  await sendMessages(bot, config);
 });
--- src/utils/sendMessages.ts
+++ src/utils/sendMessages.ts
@@ -1,12 +1,16 @@
-import { Telegraf } from "telegraf";
-import { LEXICON_EN } from "../lexicon/lexicon_en.js";
+import { Telegraf } from 'telegraf';
+import { LEXICON_EN } from '../lexicon/lexicon_en.js';
+import { getUsersArray } from './checkAccess.js';
 
-export const sendMessages = async (bot: Telegraf, allowedUserId: number[]) => {
+export const sendMessages = async (bot: Telegraf, config: Config) => {
+  const allowedUserId = await getUsersArray(config);
+
   for (const recipient of allowedUserId) {
     try {
-      await bot.telegram.sendMessage(recipient, LEXICON_EN["botStarted"]);
+      await bot.telegram.sendMessage(recipient, LEXICON_EN['botStarted']);
+      console.log(`Message sent to user: ${recipient}`)
     } catch (error) {
-      console.error(LEXICON_EN["errorSending"], recipient.toString());
+      console.error(LEXICON_EN['errorSending'], recipient.toString());
     }
   }
 };
